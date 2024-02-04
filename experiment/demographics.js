150ea430dd634c7a4883ab7654b567f5b0995f19
Dominique Makowski <dom.mak19@gmail.com>
2024-02-04 20:04:25 +0000

    more url variables

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -15,8 +15,11 @@ var demographics_browser_info = {
 
         // Add URL variables - ?sona_id=x&exp=1
         let urlvars = jsPsych.data.urlVariables()
-        data["sona_id"] = urlvars["sona_id"]
         data["researcher"] = urlvars["exp"]
+        data["sona_id"] = urlvars["sona_id"]
+        data["prolific_id"] = urlvars["PROLIFIC_PID"] // Prolific
+        data["study_id"] = urlvars["STUDY_ID"] // Prolific
+        data["session_id"] = urlvars["SESSION_ID"] // Prolific
     },
 }
 
@@ -35,27 +38,6 @@ var restrict_mobile = {
         } else {
             return false
         }
-    },
-}
-
-// Participant ID ========================================================================
-var demographics_participant_id = {
-    type: jsPsychSurveyText,
-    questions: [
-        {
-            prompt: "Enter participant ID:",
-            placeholder: "001",
-            name: "Participant_ID",
-        },
-    ],
-    data: {
-        screen: "participant_id",
-    },
-    on_finish: function () {
-        // Store `participant_id` so that it can be reused later
-        jsPsych.data.addProperties({
-            participant_id: jsPsych.data.get().last().values()[0]["response"]["Participant_ID"],
-        })
     },
 }
 
@@ -146,18 +128,31 @@ var demographics_endscreen = function (
     return {
         type: jsPsychHtmlButtonResponse,
         css_classes: ["narrow-text"],
-        stimulus:
-            "<h1>Thank you for participating</h1>" +
-            "<p>It means a lot to us. Don't hesitate to share the study by sending this link:</p>" +
-            "<p><b><a href='" +
-            link +
-            "'>" +
-            link +
-            "<a/></b></p><br>" +
-            "<h2>Information</h2>" +
-            "<p align='left'>The purpose of this study was for us to understand how Humans perceive visual illusions, and whether this relates to personality traits. Hence, this study included the Illusion Game, with measures how people's vision is biased by illusions, as well as various questionnaires that might be related.</p>" +
-            "<p align='left'>If you have any questions about the project, please contact D.Makowski@sussex.ac.uk.</p>" +
-            "<p><b>You can safely close the tab now.</b></p>",
+        stimulus: function () {
+            let text =
+                "<h1>Thank you for participating</h1>" +
+                "<p>It means a lot to us. We know participating in scientific experiments can be long and not always the most fun, so we really do appreciate your help in helping us understand how the Human brain works.</p>" +
+                "<h2>Information</h2>" +
+                "<p align='left'>The purpose of this study was for us to understand how Humans perceive visual illusions, and whether this relates to personality traits. Hence, this study included the Illusion Game, which measures how people's vision is biased by illusions, as well as various questionnaires that might be related.</p>" +
+                "<p align='left'>If you have any questions about the project, please contact <i>D.Makowski@sussex.ac.uk</i>, and check-out the <b><a href='https://realitybending.github.io/'>Reality Bending Lab</a></b> for more information about our research team.</p>" +
+                "<p align='left'>Don't hesitate to share the study by sending this link:</p>" +
+                "<p><b><a href='" +
+                link +
+                "'>" +
+                link +
+                "<a/></b></p><br>"
+
+            if (
+                jsPsych.data.get().filter({ screen: "browser_info" }).values()[0]["prolific_id"] !=
+                undefined
+            ) {
+                text +=
+                    "<br><p><b>You will now be redirected to Prolific. Please do not close this tab.</b></p>"
+            } else {
+                text += "<br><p><b>You can safely close the tab now.</b></p>"
+            }
+            return text
+        },
         choices: ["End"],
         data: { screen: "endscreen" },
     }
